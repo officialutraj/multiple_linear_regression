cd06907c94293fa7d6976c685e0f4732ebcd27b8
UTKARSH RAJ <42785251+officialutraj@users.noreply.github.com>
2019-02-09 13:11:25 +0530

    Update multiple_linear_regression.py

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -21,20 +21,28 @@ group = df.groupby('State')
 
     print(name)
 """
-
+#this block is for categorical data because in our dataset state columns have string value to change in numerical data in form of 0 and1
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder = LabelEncoder()
 X[:, 3] = labelencoder.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
+
+
+#now  data in train and test where test_size is 20%
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+
+#now fit a in linear model
 from sklearn.linear_model import LinearRegression
 reg = LinearRegression()
 reg.fit(X_train, y_train)
 
+
+
+# now prediction a X_test
 print(reg.predict(X_test))
 
 
